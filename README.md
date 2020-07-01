82525daea7a7e5e6537d707c22877c78707cb654
grayson <grayson@returntocorp.com>
2020-07-01 15:06:37 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,104 @@
+# NOTICE
+
+!!! This is a sample application that demonstrates bad email use. Do not use this. !!!
+
+A quick-and-dirty app for coordinating shared rooms for events! The app uses a style like classified ads; or the more modern variant, [Craigslist](www.craigslist.org). Hosts can post availability and seekers can post seekability.
+
+## Installation
+
+This is a Flask app, designed to be deployed anywhere a Flask app can go! When this was live, it was hosted using AWS's Elastic Beanstalk.
+
+Sorry that I don't have any instructions...!
+
+## Development
+
+I do, however, have instructions for development!
+
+1. Run DynamoDB locally.
+1. Initialize the virtual environment using `pipenv` and install dependendies.
+1. Set up environment variables.
+1. Run the app!
+
+### Run DynamoDB locally.
+
+This app uses `pynamodb` which uses an ORM-like syntax for working with [AWS's DynamoDB](https://aws.amazon.com/dynamodb/). AWS provides a [really convinient Docker image](https://hub.docker.com/r/amazon/dynamodb-local/) for testing integration with DynamoDB locally.
+
+If you don't have Docker installed, [install Docker](https://docs.docker.com/get-docker/).
+
+Pull and run the DynamoDB docker container:
+
+```bash
+$ docker pull amazon/dynamodb-local
+$ docker run -p 8000:8000 amazon/dynamodb-local
+Initializing DynamoDB Local with the following configuration:
+Port:   8000
+InMemory:       true
+DbPath: null
+SharedDb:       false
+shouldDelayTransientStatuses:   false
+CorsParams:     *
+```
+
+The local instance of DynamoDB is now running. To use the local instance of DynamoDB, you'll need to set the `app_env` environment variable. More on this is in "Set up environment variables."
+
+### Initialize the virtual environment using `pipenv` and install dependendies.
+
+This app uses `pipenv` to manage dependencies. You'll need:
+- flask
+- flask-talisman
+- pynamodb
+
+Install `pipenv` if you don't have it:
+
+```bash
+$ pip install pipenv
+```
+
+On Mac, `brew` also works:
+
+```bash
+$ brew install pipenv
+```
+
+Drop into a `pipenv shell` and install dependendies.
+
+```bash
+$ pipenv shell
+(roomshare) $ pipenv install
+```
+
+### Set up environment variables.
+
+This app needs a few things from you to run properly.
+1. The app will send emails to sign-ups on your behalf, so it will need to know the SMTP server information and a login.
+1. The app will also need to know its base URL to generate links.
+1. Lastly, the app needs to know if it is running locally or in production.
+
+You'll need to set the folloing environment variables:
+
+```bash
+export base_url="http://localhost:5000"
+export smtp_sender_email=""
+export smtp_server=""
+export smtp_username=""
+export smtp_password=""
+export app_env="LOCAL"
+```
+
+`smtp_sender_email` is the email that will be put in the `FROM:` field of emails.
+
+`smtp_server` is the SMTP server address. For example, Gmail's address is `smtp.gmail.com`.
+
+`smtp_username` is the username login for the SMTP server.
+
+`smtp_password` is the password for the username on the SMTP server.
+
+You can see how to set this up in Gmail at [this link](https://stackabuse.com/how-to-send-emails-with-gmail-using-python/).
+
+`app_env` is optional. If it is `LOCAL` it will use the local instance of DynamoDB running on port 8000. If ommitted, it will try to use your AWS credentials to create a DynamoDB table.
+
+### Run the app!
+
+```bash
+(roomshare) $ python application.py
+```
---
+++ vuln_application.py
@@ -0,0 +1,190 @@
+import logging
+import os
+import smtplib
+import sys
+import uuid
+from datetime import datetime
+from email.message import EmailMessage
+from flask import Flask, render_template, request, redirect
+from flask_talisman import Talisman
+from pynamodb.models import Model
+from pynamodb.attributes import UnicodeAttribute, NumberAttribute, UTCDateTimeAttribute
+from pynamodb.indexes import GlobalSecondaryIndex, AllProjection
+
+
+logger = logging.getLogger(__file__)
+logger.setLevel(logging.INFO)
+handler = logging.StreamHandler(stream=sys.stderr)
+handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+logger.addHandler(handler)
+
+
+application = Flask(__name__)
+Talisman(application, force_https=False)  # Fine because this will be deployed behind load balancer
+
+
+config = {
+    "base_url": os.environ['base_url'],
+    "smtp_sender_email": os.environ['smtp_sender_email'],
+    "smtp_server": os.environ['smtp_server'],
+    "smtp_username": os.environ['smtp_username'],
+    "smtp_password": os.environ['smtp_password'],
+}
+
+
+class RoleIndex(GlobalSecondaryIndex):
+    class Meta:
+        index_name = "role_index"
+        read_capacity_units = 1
+        write_capacity_units = 1
+        projection = AllProjection()
+    role = UnicodeAttribute(hash_key=True)
+
+
+class Person(Model):
+    class Meta:
+        read_capacity_units = 1
+        write_capacity_units = 1
+        table_name = "roomshare-person"
+        if os.environ.get("app_env") == "LOCAL":
+            host="http://localhost:8000"
+        else:
+            region = "us-east-1"
+    uuid = UnicodeAttribute(hash_key=True)
+    name = UnicodeAttribute()
+    email = UnicodeAttribute()
+    phone = UnicodeAttribute()
+    spots = NumberAttribute()
+    message = UnicodeAttribute()
+    role = UnicodeAttribute()
+    role_index = RoleIndex()
+    created = UTCDateTimeAttribute(default=datetime.now)
+
+
+if not Person.exists():
+    logger.info("Creating dynamodb table...")
+    Person.create_table(wait=True)
+
+
+def _send_email(uid, name, email):
+    logger.info("Sending information email to {} with uuid {}".format(email, uid))
+    delete_link = f"{config.get('base_url')}/delete?uuid={uid}"
+    from email.mime.text import MIMEText
+    from email.mime.multipart import MIMEMultipart
+
+    message = MIMEMultipart("alternative")
+    message['Subject'] = config.get('subject', 'Successful Signup for Roomshare')
+    message['From'] = config.get('smtp_sender_email', "noreply")
+    message['To'] = email
+    message.attach(MIMEText(render_template("email.email", name=name, delete_link=delete_link), "plain"))
+    message.attach(MIMEText(render_template("email.email", name=name, delete_link=delete_link), "html"))
+
+    smtp_host = config.get('smtp_server')
+
+    try:
+        logger.debug("Connecting to SMTP server {}".format(smtp_host))
+        s = smtplib.SMTP(smtp_host, 587)
+        s.connect(smtp_host, 587)
+        s.ehlo()
+        s.starttls()
+        s.ehlo()
+        logger.debug("Logging in")
+        s.login(config.get('smtp_username'), config.get('smtp_password'))
+        logger.debug("Sending email")
+        s.sendmail(config.get('smtp_sender_email', "noreply"), email, message.as_string())
+        s.close()
+    except Exception as e:
+        logger.warning("Failed to send email to {}. Error: {}".format(email, e))
+
+
+def _validate_person_request(request):
+    if not all([
+        request.form.get('name', False),
+        request.form.get('email', False) or request.form.get('phone', False)
+    ]):
+        raise ValueError("Must supply a name and either an email or phone number")
+
+
+def _add_person(request, role):
+    _validate_person_request(request)
+    uid = str(uuid.uuid4())
+    name = request.form.get('name', "")
+    email = request.form.get('email', "")
+    new_host = Person(
+        uuid=uid,
+        name=name,
+        email=email,
+        phone=request.form.get('phone', ""),
+        spots=int(request.form.get('spots')) or 1 if role == "host" else 0,
+        message=request.form.get('message', ""),
+        role=role
+    )
+    new_host.save()
+    _send_email(uid, name, email)
+
+
+def _delete_person(request):
+    target = Person.get(request.args.get('uuid'))
+    logger.debug(target)
+    target.delete()
+
+
+@application.route('/', methods=["GET"])
+def index():
+    hosts = Person.role_index.query("host")
+    splitters = Person.role_index.query("share")
+    return render_template(
+        "index.html",
+        hosts=hosts,
+        splitters=splitters,
+        company_name=config.get('company_name'),
+        contact_email=config.get('contact_email')
+    )
+
+
+@application.route('/host', methods=["POST"])
+def host():
+    try:
+        _add_person(request, "host")
+    except ValueError as ve:
+        logger.error(ve)
+        return str(ve)
+    except Exception as e:
+        logger.error("Failed to add host: {}. Error: {}".format(
+            str(request.form),
+            e)
+        )
+        return "There was a problem.  Please try again soon."
+
+    return redirect("/")
+
+
+@application.route('/share', methods=["POST"])
+def share():
+    try:
+        _add_person(request, "share")
+    except ValueError as ve:
+        logger.error(ve)
+        return str(ve)
+    except Exception as e:
+        logger.error("Failed to add share: {}. Error: {}".format(
+            str(request.form),
+            e)
+        )
+        return "There was a problem.  Please try again soon."
+
+    return redirect("/")
+
+
+@application.route('/delete', methods=["GET"])
+def delete():
+    try:
+        _delete_person(request)
+    except Exception as e:
+        logger.error("Failed to delete person {}. Error: {}".format(request.args.get("uuid", None), e))
+
+    return redirect("/")
+
+
+if __name__ == '__main__':
+    application.run()
